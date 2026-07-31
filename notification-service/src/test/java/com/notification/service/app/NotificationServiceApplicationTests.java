c0bcec022c445fae009a381da840a5bcf73f389f
manivannancsmca <manivannancsmca@gmail.com>
2026-07-31 13:03:27 +0530

    notification microservice application initiated

---
+++ notification-service/src/test/java/com/notification/service/app/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
