34b6c7ffc3f0337aa8e695f1622ac3eee6800a2b
manivannancsmca <manivannancsmca@gmail.com>
2026-07-31 13:19:20 +0530

    user entity class added

---
+++ user-service/src/main/java/com/user/service/app/entity/User.java
@@ -0,0 +1,35 @@
+package com.user.service.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, nullable = false)
+    private String username;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String fullName;
+    
+    private Boolean active;
+    
+    private LocalDateTime createdAt;
+}
